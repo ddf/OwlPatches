f2ad6db8e8161e8988c239ec404f6822c8a94162
Damien Quartz <dcomposer@gmail.com>
2021-03-28 01:26:49 -0700

    testing the ramps

--- OwlPatches/KnoscillatorLichPatch.hpp
+++ OwlPatches/KnoscillatorLichPatch.hpp
@@ -91,8 +91,8 @@ public:
     float p = knotP;
     float q = knotQ;
 
-    rotX->setFrequency(pRaw - pTarget);
-    rotY->setFrequency(qRaw - qTarget);
+    rotX->setFrequency(1);
+    rotY->setFrequency(0);
 
     float x[4], y[4], z[4];
     for(int s = 0; s < left.getSize(); ++s)
