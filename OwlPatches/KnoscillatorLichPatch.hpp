c268dc9d5d2654b57631d02462ae08fd89f8df13
Damien Quartz <dcomposer@gmail.com>
2021-04-03 21:24:58 -0700

    ported new processing code for: * doing a proper 3D -> 2D projection * rewritten parametric knot equation so we can morph coefficients instead of doing a linear interp of between three different points

--- OwlPatches/KnoscillatorLichPatch.hpp
+++ OwlPatches/KnoscillatorLichPatch.hpp
@@ -12,9 +12,22 @@ private:
   int knotP;
   int knotQ;
 
+  enum KnotType
+  {
+    TFOIL = 0,
+    LISSA = 1,
+    TORUS = 2,
+
+    KNUM = 3
+  };
+ 
+  float x1[KNUM], x2[KNUM], x3[KNUM];
+  float y1[KNUM], y2[KNUM], y3[KNUM];
+  float z1[KNUM], z2[KNUM];
+
   float phaseP;
   float phaseQ;
-  float phaseR;
+  float phaseZ;
   float phaseS;
   float phaseM;
   float phaseX;
@@ -32,7 +45,7 @@ private:
 public:
   KnoscillatorLichPatch()
     : hz(true), knotP(1), knotQ(1),
-    phaseP(0), phaseQ(0), phaseR(0), phaseS(0), phaseM(0), phaseX(0), phaseY(0),
+    phaseP(0), phaseQ(0), phaseZ(0), phaseS(0), phaseM(0), phaseX(0), phaseY(0),
     inPitch(PARAMETER_A), inMorph(PARAMETER_B), inKnotP(PARAMETER_C), inKnotQ(PARAMETER_D),
     TWO_PI(M_PI*2), oneOverSampleRate(1.0f / getSampleRate())
   {
@@ -46,6 +59,18 @@ public:
     setParameterValue(inKnotP, 0.2f);
     setParameterValue(inKnotQ, 0.2f);
 
+    x1[TFOIL] = 1; x2[TFOIL] = 2; x3[TFOIL] = 3 * M_PI / 2;
+    y1[TFOIL] = 1; y2[TFOIL] = 0; y3[TFOIL] = -2;
+    z1[TFOIL] = 1; z2[TFOIL] = 0;
+
+    x1[TORUS] = 2; x2[TORUS] = 0; /*sin(qt)*/ x3[TORUS] = 0;
+    y1[TORUS] = 1; y2[TORUS] = 0; y3[TORUS] = 0; /*cos(qt)*/
+    z1[TORUS] = 0; z2[TORUS] = 1;
+
+    x1[LISSA] = 0; x2[LISSA] = 2; x3[LISSA] = TWO_PI;
+    y1[LISSA] = 2; y2[LISSA] = M_PI * 3; y3[LISSA] = 0;
+    z1[LISSA] = 0; z2[LISSA] = 1;
+
     kpm = SineOscillator::create(getSampleRate());
     kpm->setFrequency(1.02f);
   }
@@ -55,13 +80,42 @@ public:
     SineOscillator::destroy(kpm);
   }
 
-  float sample(float* buffer, size_t bufferSize, float normIdx)
+  float interp(float* buffer, size_t bufferSize, float normIdx)
   {
     const float fracIdx = (bufferSize - 1) * normIdx;
     const int i = (int)fracIdx;
     const int j = (i + 1) % bufferSize;
     const float lerp = fracIdx - i;
     return buffer[i] + lerp * (buffer[j] - buffer[i]);
+  }
+
+  void rotate(float& x, float& y, float &z, float pitch, float yaw, float roll)
+  {
+    float cosa = cos(roll);
+    float sina = sin(roll);
+
+    float cosb = cos(pitch);
+    float sinb = sin(pitch);
+
+    float cosc = cos(yaw);
+    float sinc = sin(yaw);
+
+    float Axx = cosa * cosb;
+    float Axy = cosa * sinb*sinc - sina * cosc;
+    float Axz = cosa * sinb*cosc + sina * sinc;
+
+    float Ayx = sina * cosb;
+    float Ayy = sina * sinb*sinc + cosa * cosc;
+    float Ayz = sina * sinb*cosc - cosa * sinc;
+
+    float Azx = -sinb;
+    float Azy = cosb * sinc;
+    float Azz = cosb * cosc;
+
+    float ix = x, iy = y, iz = z;
+    x = Axx * ix + Axy * iy + Axz * iz;
+    y = Ayx * ix + Ayy * iy + Ayz * iz;
+    z = Azx * ix + Azy * iy + Azz * iz;
   }
 
   void processAudio(AudioBuffer& audio) override
@@ -88,7 +142,6 @@ public:
     float p = knotP;
     float q = knotQ;
 
-    float x[4], y[4], z[4];
     for(int s = 0; s < getBlockSize(); ++s)
     {
       float freq = hz.getFrequency(left[s]);
@@ -98,48 +151,51 @@ public:
 
       float pt = (phaseP+pm) * TWO_PI;
       float qt = (phaseQ+pm) * TWO_PI;
-      float rt = phaseR * TWO_PI;
+      float zt = phaseZ * TWO_PI;
 
       float xp = phaseX * TWO_PI;
       float yp = phaseY * TWO_PI;
       float zp = 0;
 
-      // trefoil knot
-      x[0] = sin(qt + xp) + 2 * sin(pt + xp);
-      y[0] = cos(qt + yp) - 2 * cos(pt + yp);
-      z[0] = 0.25f * sin(3 * rt + zp);
-      // torus knot
-      x[1] = cos(qt + xp) * (2.5f + cos(pt + xp));
-      y[1] = sin(qt + yp) * (2.5f + cos(pt + yp));
-      z[1] = 0.25f * sin(pt + zp);
-      // lissajous knot
-      x[2] = cos(qt + xp);
-      y[2] = cos(pt + yp);
-      z[2] = 0.5f*cos(rt + zp);
-      // trefoil again for wraparound
-      x[3] = -x[0];
-      y[3] = -y[0];
-      z[3] = z[0];
+      x2[TORUS] = sin(qt);
+      y3[TORUS] = cos(qt);
 
       phaseM += morphStep;
-      float mi = -0.5f*cos(phaseM) + 0.5f;
+      float m = -0.5f*cos(phaseM) + 0.5f;
 
-      float ox = sample(x, 4, mi);
-      float oy = sample(y, 4, mi);
-      float oz = sample(z, 4, mi);
+      float ox = interp(x1, KNUM, m)*sin(qt)                       + interp(x2, KNUM, m)*cos(pt + interp(x3, KNUM, m));
+      float oy = interp(y1, KNUM, m)*cos(qt + interp(y2, KNUM, m)) + interp(y3, KNUM, m)*cos(pt);
+      float oz = interp(z1, KNUM, m)*sin(3 * zt)                   + interp(z2, KNUM, m)*sin(pt);
 
-      left[s] = ox * oz;
-      right[s] = oy * oz;
+      rotate(ox, oy, oz, phaseX*TWO_PI, phaseY*TWO_PI, 0);
+
+      // #TODO squiggle
+      //float sqp = (phaseSq + spm.getLastValue())*TWO_PI;
+      //float sqa = 0.01f * (s.getLastValue() - 1);
+      //ox += sqa * cos(sqp);
+      //oy += sqa * sin(sqp);
+
+      const int camDist = 6;
+      float projection = 1.0f / (oz + camDist);
+      left[s]  = ox * projection;
+      right[s] = oy * projection;
 
       float step = freq * oneOverSampleRate;
-      phaseR += step;
-      if (phaseR > 1) phaseR -= 1;
+      phaseZ += step;
+      if (phaseZ > 1) phaseZ -= 1;
 
       phaseQ += step * q + step;
       if (phaseQ > 1) phaseQ -= 1;
 
       phaseP += step * p;
       if (phaseP > 1) phaseP -= 1;
+
+      // #TODO squiggle
+      //phaseSq += stepSize * 8 * (p.getLastValue() + q.getLastValue()) * (1 + srt.getLastValue());
+      //if (phaseSq > 1)
+      //{
+      //  phaseSq -= 1;
+      //}
 
       phaseX += oneOverSampleRate*rotateBaseFreq*(pRaw-1)*freq;
       if (phaseX > 1) phaseX -= 1;
