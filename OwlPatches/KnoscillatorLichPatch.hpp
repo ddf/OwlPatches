82f873eacfd75958a3b9968e3c2fb6f7861db6e0
Damien Quartz <dcomposer@gmail.com>
2021-04-19 20:25:37 -0700

    Comment with attribution, license, and description.

--- OwlPatches/KnoscillatorLichPatch.hpp
+++ OwlPatches/KnoscillatorLichPatch.hpp
@@ -1,4 +1,34 @@
-  
+/**
+
+AUTHOR:
+    (c) 2021 Damien Quartz
+
+LICENSE:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+DESCRIPTION:
+    Knoscillator Lich is a stereo oscillator that oscillates over a 3D curve (or Knot).
+    The Knot can morphed between three Knot equations based on the Trefoil Knot,
+    Lissajous Curve, and Torus Knot. Each 3D sample is projected to a 2D point
+    whose X-Y coordinates are used as the left and right audio outputs. By plotting
+    the audio on a scope in X-Y mode, you will be able to see the Knot generating the sound.
+    The Knot shape can be changed by adjusting the P and Q coefficients
+    and it rotates around the X and Y axes at speeds relative to P and Q,
+    which generates a ever-changing stereo field.
+*/
+
 #include "Patch.h"
 #include "MidiMessage.h"
 #include "VoltsPerOctave.h"
