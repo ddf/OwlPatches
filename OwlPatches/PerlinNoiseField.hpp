700ee5eb93d284032cea92972a7f2c417bb66b63
Damien Quartz <dcomposer@gmail.com>
2021-04-25 15:41:08 -0700

    patch that processes audio by running it through a perlin noise field, similar to how noise is applied in knoscillator

---
+++ OwlPatches/PerlinNoiseField.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "SignalProcessor.h"
+#include "Patch.h"
+#include "Noise.hpp"
+
+class PerlinNoiseField : public MultiSignalProcessor
+{
+  float frequency;
+  unsigned int depth;
+  float offsetX, offsetY;
+
+  PerlinNoiseField()
+    : frequency(1), depth(1), offsetX(0), offsetY(0)
+  {
+
+  }
+
+public:
+  static PerlinNoiseField* create()
+  {
+    return new PerlinNoiseField();
+  }
+
+  static void destroy(PerlinNoiseField* pnf) 
+  {
+    delete pnf;
+  }
+
+  void setFrequency(float freq)
+  {
+    frequency = max(0, freq);
+  }
+
+  void setDepth(int depth)
+  {
+    depth = max(1, depth);
+  }
+
+  void setOffsetX(float offset)
+  {
+    offsetX = max(0, offset);
+  }
+
+  void setOffsetY(float offset)
+  {
+    offsetY = max(0, offset);
+  }
+
+  void process(AudioBuffer& input, AudioBuffer& output) override
+  {
+    FloatArray xin = input.getSamples(LEFT_CHANNEL);
+    FloatArray yin = input.getChannels() >= 2 ? input.getSamples(RIGHT_CHANNEL) : xin;
+    FloatArray outL = output.getSamples(LEFT_CHANNEL);
+    FloatArray outR = output.getChannels() >= 2 ? output.getSamples(RIGHT_CHANNEL) : outL;
+    const int blockSize = min(input.getSize(), output.getSize());
+    for (int i = 0; i < blockSize; ++i)
+    {
+      float leftIn = xin[i];
+      float rightIn = yin[i];
+      float x = leftIn * 0.5f + 0.5f;
+      float y = rightIn * 0.5f + 0.5f;
+      float nz = perlin2d(x + offsetX, y + offsetY, frequency, depth);
+      outL[i] = leftIn + leftIn * nz;
+      outR[i] = rightIn + rightIn * nz;
+    }
+  }
+
+};
---
+++ OwlPatches/PerlinNoiseFieldLichPatch.hpp
@@ -0,0 +1,42 @@
+#include "Patch.h"
+#include "PerlinNoiseField.hpp"
+
+class PerlinNoiseFieldLichPatch : public Patch
+{
+  PerlinNoiseField* noiseField;
+
+  static const PatchParameterId inNoiseFrequency = PARAMETER_A;
+  static const PatchParameterId inNoiseDepth = PARAMETER_B;
+  static const PatchParameterId inOffsetX = PARAMETER_C;
+  static const PatchParameterId inOffsetY = PARAMETER_D;
+
+public:
+  PerlinNoiseFieldLichPatch()
+  {
+    noiseField = PerlinNoiseField::create();
+    registerParameter(inNoiseFrequency, "Noise Frequency");
+    registerParameter(inNoiseDepth, "Noise Depth");
+    registerParameter(inOffsetX, "X Offset");
+    registerParameter(inOffsetY, "Y Offset");
+
+    setParameterValue(inNoiseFrequency, 0);
+    setParameterValue(inNoiseDepth, 0);
+    setParameterValue(inOffsetX, 0);
+    setParameterValue(inOffsetY, 0);
+  }
+
+  ~PerlinNoiseFieldLichPatch()
+  {
+    PerlinNoiseField::destroy(noiseField);
+  }
+
+  void processAudio(AudioBuffer& audio) override
+  {
+    noiseField->setOffsetX(getParameterValue(inOffsetX));
+    noiseField->setOffsetY(getParameterValue(inOffsetY));
+    noiseField->setFrequency(getParameterValue(inNoiseFrequency) * 16 + 1);
+    noiseField->setDepth(getParameterValue(inNoiseDepth) * 8 + 1);
+    noiseField->process(audio, audio);
+  }
+
+};
