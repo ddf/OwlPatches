8eca8f89b48ca1f988acb5a3294adbdae16f116f
Damien Quartz <dcomposer@gmail.com>
2021-10-24 20:00:09 -0700

    add back right channel output in grains

--- OwlPatches/Grain.hpp
+++ OwlPatches/Grain.hpp
@@ -123,7 +123,7 @@ public:
       const float env = envelope();
 
       //*outL++ += interpolated(left, i, j, t) * env * leftScale;
-      //*outR++ += interpolated(right, i, j, t) * env * rightScale;
+      *outR++ += interpolated(right, i, j, t) * env * rightScale;
 
       // keep looping, but silently, mainly so we can keep track of grain performance
       // just this on its own is about 6ns per grain
