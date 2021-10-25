0eeb4a9ddd1e89afbec2bbd39a9f93e63d54eae8
Damien Quartz <dcomposer@gmail.com>
2021-10-24 19:33:12 -0700

    remove modulos from generate inner loop

--- OwlPatches/Grain.hpp
+++ OwlPatches/Grain.hpp
@@ -112,13 +112,15 @@ public:
       --outLen;
     }
 
+    const int wrapAt = bufferSize - 1;
     while(outLen--)
     {
       const float pos = start + ramp;
-      const float t = pos - (int)pos;
-      const int i = ((int)pos) % bufferSize;
-      const int j = (i + 1) % bufferSize;
-      //const float env = envelope();
+      const int ipos = (int)pos;
+      const float t = pos - ipos;
+      const int i = ipos > wrapAt ? ipos - bufferSize : ipos;
+      const int j = i == wrapAt ? 0 : i + 1;
+      const float env = envelope();
 
       //*outL++ += interpolated(left, i, j, t) * env * leftScale;
       //*outR++ += interpolated(right, i, j, t) * env * rightScale;
