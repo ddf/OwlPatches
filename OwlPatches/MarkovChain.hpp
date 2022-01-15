15f2593248bb2f51f059e5acd4e998eb4186211b
Damien Quartz <dcomposer@gmail.com>
2022-01-15 14:37:44 -0800

    first pass on markov chain patch: listen to input to "learn" how to make sound, generate output from learned input

---
+++ OwlPatches/MarkovChain.hpp
@@ -0,0 +1,91 @@
+#include "SignalGenerator.h"
+#include "basicmaths.h"
+
+#define FLOAT_TO_UNSIGNED 65535
+#define UNSIGNED_TO_FLOAT (1.0f/65535)
+
+#define UNSIGNED(x) ((uint16_t)((x*0.5f + 0.5f)*FLOAT_TO_UNSIGNED))
+#define FLOAT(x)    ((x*UNSIGNED_TO_FLOAT)*2 - 1)
+
+#define MEMORY_PER_SAMPLE 8 // must be power of 2
+
+template<int SIZE>
+struct SampleMemory
+{
+  uint16_t samples[SIZE];
+  uint16_t writePosition;
+
+  void write(uint16_t sample)
+  {
+    samples[writePosition] = sample;
+    writePosition = (writePosition + 1) & (SIZE - 1);
+  }
+};
+
+class MarkovChain : public SignalGenerator
+{
+  typedef SampleMemory<MEMORY_PER_SAMPLE> MemType;
+
+  MemType* memory;
+  uint16_t lastLearn;
+  uint16_t lastGenerate;
+
+public:
+  MarkovChain()
+    : memory(0), lastLearn(0), lastGenerate(0)
+  {
+    memory = new MemType[FLOAT_TO_UNSIGNED];
+    memset(memory, 0, FLOAT_TO_UNSIGNED * sizeof(MemType));
+  }
+
+  ~MarkovChain()
+  {
+    if (memory)
+      delete[] memory;
+  }
+
+  void setLastLearn(float value)
+  {
+    lastLearn = UNSIGNED(value);
+  }
+
+  void setLastGenerate(float value)
+  {
+    lastGenerate = UNSIGNED(value);
+  }
+
+  void learn(FloatArray input)
+  {
+    for (int i = 0, sz = input.getSize(); i < sz; ++i)
+    {
+      uint16_t sample = UNSIGNED(input[i]);
+      memory[lastLearn].write(sample);
+      lastLearn = sample;
+    }
+  }
+
+  float generate() override
+  {
+    lastGenerate = memory[lastGenerate].samples[rand()&(MEMORY_PER_SAMPLE - 1)];
+    return FLOAT(lastGenerate);
+  }
+
+  void generate(FloatArray output) override
+  {
+    for (int i = 0; int sz = output.getSize(); ++i)
+    {
+      output[i] = generate();
+    }
+  }
+
+  static MarkovChain* create()
+  {
+    return new MarkovChain();
+  }
+
+  static void destroy(MarkovChain* markov)
+  {
+    if (markov)
+      delete markov;
+  }
+};
---
+++ OwlPatches/MarkovPatch.hpp
@@ -0,0 +1,104 @@
+/**
+
+AUTHOR:
+    (c) 2022 Damien Quartz
+
+LICENSE:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+DESCRIPTION:
+
+*/
+
+#include "Patch.h"
+#include "MarkovChain.hpp"
+
+class MarkovPatch : public Patch 
+{
+  MarkovChain* markov;
+  uint16_t listening;
+  uint16_t generating;
+
+  float lastLearnLeft, lastLearnRight;
+  float lastGenLeft, lastGenRight;
+
+  static const PatchButtonId inToggleListen = BUTTON_1;
+  static const PatchButtonId inToggleGenerate = BUTTON_2;
+
+public: 
+  MarkovPatch()
+  : listening(OFF), generating(OFF), lastLearnLeft(0), lastLearnRight(0)
+  , lastGenLeft(0), lastGenRight(0)
+  {
+    markov = MarkovChain::create();
+  }
+
+  ~MarkovPatch()
+  {
+    MarkovChain::destroy(markov);
+  }
+
+  void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples) override
+  {
+    if (bid == inToggleListen && value == ON)
+    {
+      listening = listening == ON ? OFF : ON;
+      if (listening)
+      {
+        lastLearnLeft = 0;
+        lastLearnRight = 0;
+      }
+    }
+    else if (bid == inToggleGenerate && value == ON)
+    {
+      generating = generating == ON ? OFF : ON;
+      if (generating)
+      {
+        lastGenLeft = 0;
+        lastGenRight = 0;
+      }
+    }
+  }
+
+  void processAudio(AudioBuffer& audio) override
+  {
+    FloatArray left = audio.getSamples(0);
+    FloatArray right = audio.getSamples(0);
+
+    if (listening)
+    {
+      markov->setLastLearn(lastLearnLeft);
+      markov->learn(left);
+      markov->setLastLearn(lastLearnRight);
+      markov->learn(right);
+      lastLearnLeft = left[left.getSize() - 1];
+      lastLearnRight = right[right.getSize() - 1];
+    }
+
+    if (generating)
+    {
+      markov->setLastGenerate(lastGenLeft);
+      markov->generate(left);
+      markov->setLastGenerate(lastGenRight);
+      markov->generate(right);
+      lastGenLeft = left[left.getSize() - 1];
+      lastGenRight = right[right.getSize() - 1];
+    }
+
+    setButton(inToggleListen, listening);
+    setButton(inToggleGenerate, generating);
+  }
+  
+};
