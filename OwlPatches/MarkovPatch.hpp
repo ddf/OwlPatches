b3d8f4622b9958232bf9425af2e6d18e11262af8
Damien Quartz <dcomposer@gmail.com>
2022-01-15 18:23:50 -0800

    start generating from 0 when enabling generation

--- OwlPatches/MarkovPatch.hpp
+++ OwlPatches/MarkovPatch.hpp
@@ -48,6 +48,8 @@ public:
   , genBuffer(0), lastGenLeft(0), lastGenRight(0)
   {
     markov = MarkovChain::create();
+    markov->setLastGenerate(0);
+    markov->setLastLearn(0);
     genBuffer = AudioBuffer::create(2, getBlockSize());
 
     registerParameter(inDryWet, "Dry/Wet");
@@ -76,8 +78,7 @@ public:
       generating = generating == ON ? OFF : ON;
       if (generating)
       {
-        lastGenLeft = 0;
-        lastGenRight = 0;
+        markov->setLastGenerate(0);
       }
       else
       {
