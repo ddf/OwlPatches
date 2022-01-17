630982a10fa0561748d93b9c9a9596d9a514b5fc
Damien Quartz <dcomposer@gmail.com>
2022-01-16 21:14:56 -0800

    see how it behaves with fewer nodes

--- OwlPatches/MarkovChain.hpp
+++ OwlPatches/MarkovChain.hpp
@@ -3,7 +3,7 @@
 
 typedef float Sample;
 #define MEMORY_SIZE (1<<15)
-#define MEMORY_MAX_NODES MEMORY_SIZE*5
+#define MEMORY_MAX_NODES MEMORY_SIZE*2
 #define MEMORY_PER_SAMPLE 4
 #define JITTER 0.000001f
 
