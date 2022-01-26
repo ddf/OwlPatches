f650216947914ecbbaed8fad711e52d389d523be
Damien Quartz <dcomposer@gmail.com>
2022-01-25 18:57:54 -0800

    use X as the key for stereo sample frames to test hypothesis about clicks

--- OwlPatches/MarkovChain.hpp
+++ OwlPatches/MarkovChain.hpp
@@ -269,7 +269,7 @@ protected:
     Frame(S _x, S _y) : x(_x), y(_y) {}
     S left() const { return x; }
     S right() const { return y; }
-    S key() const { return x*0.5f + y*0.5f; }
+    S key() const { return x; } // x*0.5f + y * 0.5f; }
   };
 
   typedef Frame<Sample, channels> SampleFrame;
