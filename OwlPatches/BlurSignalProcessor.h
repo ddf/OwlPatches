b17f39361d2d852d406ffa11d487604830d2a7e5
Damien Quartz <dcomposer@gmail.com>
2022-02-13 15:57:11 -0800

    implement the blur processor

--- OwlPatches/BlurSignalProcessor.h
+++ OwlPatches/BlurSignalProcessor.h
@@ -1,5 +1,6 @@
 #include "SignalProcessor.h"
 #include "CircularTexture.h"
+#include "basicmaths.h"
 
 enum BlurAxis
 {
@@ -12,18 +13,53 @@ class BlurSignalProcessor : public SignalProcessor
 {
 protected:
   CircularFloatTexture texture;
+  float size = 0.2f;
+  int samples = 10;
+
+private:
+  float standardDev;
+  float standardDevSq;
+  float gaussCoeff;
 
 public:
   BlurSignalProcessor() {}
   BlurSignalProcessor(float* textureData, int textureSizeX, int textureSizeY)
     : texture(textureData, textureSizeX, textureSizeY)
   {
+    setStandardDeviation(0.02f);
+  }
 
+  void setStandardDeviation(float value)
+  {
+    standardDev = std::max(value, 0.01f);
+    standardDevSq = standardDev * standardDev;
+    gaussCoeff = 1.0f / sqrtf(2 * M_PI*standardDevSq);
   }
 
   float process(float input) override
   {
-    return input;
+    texture.write(input);
+
+    float sum = 0;
+    float c = size * 0.5f;
+    float v = 0;
+
+    for (float s = 0; s < samples; ++s)
+    {
+      float offset = (s / (samples - 1) - 0.5f)*size;
+      float gaussWeight = gaussCoeff * pow(M_E, -((offset*offset) / (2 * standardDevSq)));
+      sum += gaussWeight;
+      if (AXIS == AxisX)
+      {
+        v += texture.read(c + offset, 0) * gaussWeight;
+      }
+      else
+      {
+        v += texture.read(0, c + offset) * gaussWeight;
+      }
+    }
+
+    return v / sum;
   }
 
   using SignalProcessor::process;
--- OwlPatches/CircularTexture.h
+++ OwlPatches/CircularTexture.h
@@ -1,4 +1,5 @@
 #include "CircularBuffer.h"
+#include "Interpolator.h"
 
 template<typename DataType, typename IndexType = size_t>
 class CircularTexture
@@ -15,6 +16,37 @@ public:
   DataType* getData() 
   {
     return buffer.getData();
+  }
+
+  void write(DataType value)
+  {
+    buffer.write(value)
+  }
+
+  DataType read(IndexType x, IndexType y)
+  {
+    IndexType index = buffer.getWriteHead() - 1 - (y*sizeX + x);
+
+    while (index < 0) index += buffer.getSize();
+
+    return buffer.readAt(index);
+  }
+
+  public float read(float u, float v)
+  {
+    float x = u * sizeX;
+    IndexType x1 = IndexType(x);
+    IndexType x2 = x1 + 1;
+    float xt = x - x1;
+
+    float y = v * sizeY;
+    IndexType y1 = IndexType(y);
+    IndexType y2 = y1 + 1;
+    float yt = y - y1;
+
+    float xv1 = Interpolator::linear(read(x1, y1), read(x2, y1), xt);
+    float xv2 = Interpolator::linear(read(x1, y2), read(x2, y2), xt);
+    return Interpolator::linear(xv1, xv2, yt);
   }
 };
 
