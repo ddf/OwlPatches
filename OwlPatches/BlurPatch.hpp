b48ff7572c26871fe9ed8c5b53119a1df01b3cc9
Damien Quartz <dcomposer@gmail.com>
2022-02-27 15:53:40 -0800

    increase blur sizes

--- OwlPatches/BlurPatch.hpp
+++ OwlPatches/BlurPatch.hpp
@@ -68,8 +68,8 @@ class BlurPatch : public Patch
   static const int minTextureSize = 16 / blurResampleFactor;
   static const int maxTextureSize = 256 / blurResampleFactor;
   const  float maxBlurSamples     = 31.0f / blurResampleFactor;
-  const  float minBlurSize        = 0.1f;
-  const  float maxBlurSize        = 0.9f;
+  const  float minBlurSize        = 0.2f;
+  const  float maxBlurSize        = 0.98f;
 
   // maximum standard deviation was chosen based on the recommendation here:
   // https://dsp.stackexchange.com/questions/10057/gaussian-blur-standard-deviation-radius-and-kernel-size
