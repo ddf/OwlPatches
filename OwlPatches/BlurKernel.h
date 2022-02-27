1ee6175a56f1dd68777a2f3d049b84cec190af88
Damien Quartz <dcomposer@gmail.com>
2022-02-27 10:55:54 -0800

    switch back to offsets centered on zero

--- OwlPatches/BlurKernel.h
+++ OwlPatches/BlurKernel.h
@@ -40,7 +40,7 @@ public:
 
     for (int s = 0; s < size; ++s)
     {
-      float offset = ((float)s / (size - 1))*blurSize*0.5f;
+      float offset = ((float)s / (size - 1) - 0.5f)*blurSize;
       float gaussWeight = gaussCoeff * pow(M_E, -((offset*offset) / (2 * standardDevSq)));
       data[s] = BlurKernelSample(offset, gaussWeight);
       sum += gaussWeight;
