64d573c9638cdba29c50ef1a03072a2b138c0621
Damien Quartz <dcomposer@gmail.com>
2022-04-18 15:59:24 -0700

    added parameters for some things, switched back to exponential decay envelope for the chirps. not so sure this patch is all that interesting in the end.

--- OwlPatches/CreaturePatch.hpp
+++ OwlPatches/CreaturePatch.hpp
@@ -12,7 +12,7 @@ class CreaturePatch : public MonochromeScreenPatch
 {
   using Delay = FractionalDelayProcessor<LINEAR_INTERPOLATION>;
   using SawOscillator = InvertedRampOscillator;
-  using DecayEnvelope = AdsrEnvelope<true>;
+  using DecayEnvelope = ExponentialDecayEnvelope;
 
   struct Chirp
   {
@@ -20,6 +20,24 @@ class CreaturePatch : public MonochromeScreenPatch
     DecayEnvelope* decay;
     float finc;
   };
+
+  static const PatchParameterId inPitch = PARAMETER_A;
+  static const PatchParameterId inDecay = PARAMETER_B;
+  static const PatchParameterId inWobble = PARAMETER_C;
+  static const PatchParameterId inEcho = PARAMETER_D;
+  static const PatchParameterId inWidth = PARAMETER_E;
+
+  static const PatchParameterId outAmp = PARAMETER_F;
+  static const PatchParameterId outFreq = PARAMETER_G;
+
+  const float pitchLow = 60;
+  const float pitchHigh = 60 * 8;
+
+  const float decayMinLow = 0.01f;
+  const float decayMaxLow = 0.03f;
+  const float decayMinHigh = 2.9f;
+  const float decayMaxHigh = 3.1f;
+
 
   static const int kChirpCount = 8;
   Chirp chirps[kChirpCount];
@@ -31,15 +49,20 @@ class CreaturePatch : public MonochromeScreenPatch
   Delay* freqDelay;
   WaveShaper* freqDelayShaper;
   FloatArray freqDelayShaperTable;
-  float waveCenterFrequency;
   float ampDelayValue;
   float freqDelayValue;
   PatchButtonId lastButtonPress = BUTTON_1;
 
+  SmoothFloat pitch;
+  SmoothFloat decay;
+  SmoothFloat wobble;
+  SmoothFloat echo;
+  SmoothFloat width;
+
 public:
   CreaturePatch() 
     : MonochromeScreenPatch()
-    , waveCenterFrequency(60), ampDelayValue(0), freqDelayValue(0)
+    , pitch(0.9f, pitchLow), ampDelayValue(0), freqDelayValue(0)
   {
     for (int i = 0; i < kChirpCount; ++i)
     {
@@ -49,7 +72,7 @@ public:
     }
 
     wave = SineOscillator::create(getSampleRate());
-    wave->setFrequency(waveCenterFrequency);
+    wave->setFrequency(pitch);
 
     pan = SineOscillator::create(getSampleRate());
     pan->setFrequency(1.5f);
@@ -68,8 +91,14 @@ public:
     freqDelayShaperTable.subArray(768, 256).ramp(3000, 0);
     freqDelayShaper = WaveShaper::create(freqDelayShaperTable);
 
-    registerParameter(PARAMETER_A, "Amp Delay>");
-    registerParameter(PARAMETER_B, "Freq Delay>");
+    registerParameter(inPitch, "Pitch");
+    registerParameter(inDecay, "Decay");
+    registerParameter(inWobble, "Wobble");
+    registerParameter(inEcho, "Echo");
+    registerParameter(inWidth, "Width");
+
+    registerParameter(outAmp, "Amp>");
+    registerParameter(outFreq, "Freq>");
   }
 
   ~CreaturePatch()
@@ -94,28 +123,32 @@ public:
     if ((bid == BUTTON_1 || bid == BUTTON_2) && value == Patch::ON)
     {
       lastButtonPress = bid;
-      const float lowFreq = bid == BUTTON_1 ? 10.f : 1000.0f;
-      const float hiFreq  = bid == BUTTON_1 ? 80.f : 8000.0f;
-      for (int i = 0; i < kChirpCount; ++i)
+      const float lowFreq = Interpolator::linear(10.f, 1000.0f, pitch);
+      const float hiFreq = Interpolator::linear(80.f, 8000.0f, pitch);
+      const float durMin = Interpolator::linear(decayMinLow, decayMinHigh, decay);
+      const float durMax = Interpolator::linear(decayMaxLow, decayMaxHigh, decay);
+      // find chirp with lowest level and retrigger it
+      int cidx = 0;
+      for (int i = 1; i < kChirpCount; ++i)
       {
-        Chirp& chirp = chirps[i];
-        if (chirp.decay->getLevel() == 0)
+        if (chirps[i].decay->getLevel() < chirps[cidx].decay->getLevel())
         {
-          float dur = Interpolator::linear(0.01f, 0.03f, randf());
-          float freq = Interpolator::linear(lowFreq, hiFreq, randf());
-          float fromFreq = freq * Interpolator::linear(0.8f, 1.2f, randf());
-          float toFreq = freq * Interpolator::linear(0.8f, 1.2f, randf());
-          chirp.osc->setFrequency(fromFreq);
-          chirp.finc = (toFreq - fromFreq) / (dur * getSampleRate());
-          chirp.osc->reset();
-          chirp.decay->setDecay(dur);
-          chirp.decay->setSustain(0);
-          chirp.decay->trigger();
-          // pull one sample so that the level is not 0 in process audio
-          chirp.decay->generate();
-          break;
+          cidx = i;
         }
       }
+      Chirp& chirp = chirps[cidx];
+      float dur = Interpolator::linear(durMin, durMax, randf());
+      float freq = Interpolator::linear(lowFreq, hiFreq, randf());
+      float fromFreq = freq * Interpolator::linear(0.8f, 1.2f, randf());
+      float toFreq = freq * Interpolator::linear(0.8f, 1.2f, randf());
+      chirp.osc->setFrequency(fromFreq);
+      chirp.finc = (toFreq - fromFreq) / (dur * getSampleRate());
+      chirp.osc->reset();
+      chirp.decay->setDecay(dur);
+      //chirp.decay->setSustain(0);
+      chirp.decay->trigger();
+      // pull one sample so that the level is not 0 in process audio
+      chirp.decay->generate();
     }
   }
 
@@ -125,36 +158,43 @@ public:
     FloatArray left = audio.getSamples(0);
     FloatArray right = audio.getSamples(1);
 
+    pitch = getParameterValue(inPitch); // Interpolator::linear(pitchLow, pitchHigh, getParameterValue(inPitch));
+    decay = getParameterValue(inDecay);
+    wobble = getParameterValue(inWobble)*0.95f;
+    echo = getParameterValue(inEcho)*0.95f;
+    width = getParameterValue(inWidth);
+
+    //wave->setFrequency(pitch);
+
     for (int i = 0; i < size; ++i)
     {
       float chirpSignal = 0;
       for (int c = 0; c < kChirpCount; ++c)
       {
         Chirp& chirp = chirps[c];
-        if (chirp.decay->getLevel() > 0)
+        if (chirp.decay->getLevel() > 0.0001f)
         {
           chirpSignal += chirp.osc->generate() * chirp.decay->generate() * 0.6f;
           chirp.osc->setFrequency(chirp.osc->getFrequency() + chirp.finc);
         }
       }
 
-      float freqDelaySamples = (0.1f + ampDelayValue*0.01f)*getSampleRate();
+      float freqDelaySamples = (0.05f + ampDelayValue*0.02f)*getSampleRate();
       freqDelay->setDelay(freqDelaySamples);
-      freqDelayValue = freqDelay->process(chirpSignal + freqDelayValue * 0.2f); // *wave->getSample();
+      freqDelayValue = freqDelay->process(chirpSignal + freqDelayValue * wobble); // *wave->getSample();
 
       float delayModFM = fabsf(freqDelayValue * wave->getSample());
       delayMod->setFrequency(delayModFM);
       float ampDelaySamples = (delayMod->generate()*0.1f + 0.15f) * getSampleRate();
       ampDelay->setDelay(ampDelaySamples);
-      ampDelayValue = clamp(ampDelay->process(chirpSignal + ampDelayValue*0.25f), -1.0f, 1.0f);
+      ampDelayValue = clamp(ampDelay->process(chirpSignal + ampDelayValue*echo), -1.0f, 1.0f);
 
-      //float waveFreq = waveCenterFrequency + freqDelayValue * 0.5f * waveCenterFrequency;
       float waveFreq = freqDelayShaper->process(freqDelayValue);
       //wave->setFrequency(waveFreq);
       pan->setFrequency(Interpolator::linear(0.2f, 10.f, freqDelayValue*0.5f + 0.5f));
 
       float waveValue = wave->generate(waveFreq / getSampleRate()) * ampDelayValue;
-      float panValue = pan->generate() * 0.1f;
+      float panValue = pan->generate() * width;
 
       float normBalance = (panValue + 1.f) * 0.5f;
 
@@ -166,8 +206,8 @@ public:
       right[i] = rightAmp * waveValue;
     }
 
-    setParameterValue(PARAMETER_A, ampDelayValue*0.5f + 0.5f);
-    setParameterValue(PARAMETER_B, clamp(freqDelayValue*0.5f + 0.5f, 0.0, 1.0f));
+    setParameterValue(outAmp, ampDelayValue*0.5f + 0.5f);
+    setParameterValue(outFreq, clamp(freqDelayValue*0.5f + 0.5f, 0.0, 1.0f));
   }
 
   void processScreen(MonochromeScreenBuffer& screen) override
