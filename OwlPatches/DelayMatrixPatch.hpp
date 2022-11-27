f4f9403e17447e9b9bdd157f7829cde09bfdc2c6
Damien Quartz <dcomposer@gmail.com>
2022-11-26 16:00:00 -0800

    Change delay gate out to PUSHBUTTON instead of BUTTON_1 because on Lich PUSHBUTTON is how we set the Gate Out and on Genius this also works for setting Gate 1 Out.

--- OwlPatches/DelayMatrixPatch.hpp
+++ OwlPatches/DelayMatrixPatch.hpp
@@ -611,7 +611,7 @@ public:
 
     setParameterValue(patchParams.lfoOut, clamp(lfoGen*0.5f + 0.5f, 0.f, 1.f));
     setParameterValue(patchParams.rndOut, clamp(rndGen*0.5f + 0.5f, 0.f, 1.f));
-    setButton(BUTTON_1, delayGate);
+    setButton(PUSHBUTTON, delayGate);
     setButton(BUTTON_2, freezeState == FreezeOn ? 1 : 0);
 
 #ifdef PROFILE
