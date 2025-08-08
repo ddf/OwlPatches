0c08d629809f890140d549515e0d2665b96c090d
Damien Quartz <dcomposer@gmail.com>
2025-08-07 23:47:11 -0700

    adding ramp to the test patch

--- OwlPatches/VesslTestPatch.hpp
+++ OwlPatches/VesslTestPatch.hpp
@@ -1,37 +1,70 @@
 #pragma once
 
-#include "Patch.h"
+#include "MonochromeScreenPatch.h"
 #include "VoltsPerOctave.h"
 #include "vessl/vessl.h"
 
 using Oscil = vessl::oscil<float, vessl::waves::sine<float>>;
+using Ramp = vessl::ramp<float>;
 
-class VesslTestPatch final : public Patch
+class VesslTestPatch final : public MonochromeScreenPatch
 {
   Oscil osc;
   VoltsPerOctave voct;
-  
+  Ramp ramp;
 public:
-  VesslTestPatch() : osc(getSampleRate()), voct(true)
+  VesslTestPatch() : osc(getSampleRate()), voct(true), ramp(getSampleRate(), 0, 1, 0)
   {
-    for (int i = 0; i < osc.getParameterCount(); ++i)
+    registerParameter(PARAMETER_A, ramp.duration().name());
+    setParameterValue(PARAMETER_A, 0.1f);
+    
+    for (int i = 0; i < osc.getSize(); ++i)
     {
-      registerParameter(static_cast<PatchParameterId>(i), osc.getParameter(i).name());
+      registerParameter(static_cast<PatchParameterId>(PARAMETER_B + i), osc[i].name());
     }
+
+    ramp.trigger();
   }
   
   void processAudio(AudioBuffer& audio) override
   {
-    osc.fHz().write(60 + getParameterValue(PARAMETER_A)*4000);
+    ramp.duration() << getParameterValue(PARAMETER_A);
+    osc.fHz() << 60 + getParameterValue(PARAMETER_B)*4000;
     FloatArray left = audio.getSamples(LEFT_CHANNEL);
     FloatArray right = audio.getSamples(RIGHT_CHANNEL);
+    uint16_t eorState = OFF;
+    uint16_t eorIndex = 0;
     for (int i = 0; i < audio.getSize(); ++i)
     {
-      osc.pm().write(left[i]);
-      osc.fmExp().write(voct.sampleToVolts(right[i]));
-      float s = osc.generate();
+      osc.pm() << left[i];
+      osc.fmExp() << voct.sampleToVolts(right[i]);
+      float s = osc.generate() * ramp.generate();
       left[i] = s;
       right[i] = s;
+      if (eorState == OFF && *ramp.eor() > 0)
+      {
+        eorState = ON;
+        eorIndex = i;
+      }
     }
+
+    setButton(BUTTON_1, eorState, eorIndex);
+  }
+
+  void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples) override
+  {
+    if (bid == BUTTON_1 && value == ON)
+    {
+      ramp.trigger();
+    }
+  }
+  
+  void processScreen(MonochromeScreenBuffer& screen) override
+  {
+    screen.setCursor(0, 10);
+    //screen.setTextSize(2);
+    // @todo this doesn't work for some reason, something is getting lost with the use of unitInit
+    screen.print(osc.name());
+    //screen.print("hello world");
   }
 };
