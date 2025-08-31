373921f0b5cdb581a340815dbb9cc884fa50f414
Damien Quartz <dcomposer@gmail.com>
2025-08-30 18:27:20 -0700

    added high pass filtering to the feedback signal

--- OwlPatches/Gauss.h
+++ OwlPatches/Gauss.h
@@ -6,7 +6,9 @@
 using GaussProcessor = GaussianBlurSignalProcessor<float>;
 using Smoother = vessl::smoother<float>;
 using vessl::parameter;
-using GaussSampleFrame = vessl::stereo<float>;
+using GaussSampleFrame = vessl::frame::stereo<float>;
+using FilterType = vessl::filtering::biquad::df2T<float, vessl::filtering::biquad::hp<1>>;
+using HighPass = vessl::filter<float, FilterType>;
 
 class Gauss : public vessl::unitProcessor<GaussSampleFrame>
 {
@@ -23,6 +25,8 @@ class Gauss : public vessl::unitProcessor<GaussSampleFrame>
   array<BlurKernel> blurKernels;
   GaussProcessor* processorLeft;
   GaussProcessor* processorRight;
+  HighPass feedbackFilterLeft;
+  HighPass feedbackFilterRight;
   Smoother textureSizeSmoother;
   Smoother blurSizeSmoother;
   Smoother feedbackAmountSmoother;
@@ -44,6 +48,8 @@ class Gauss : public vessl::unitProcessor<GaussSampleFrame>
 public:
   explicit Gauss(float sampleRate, int blockSize) : unitProcessor(init, sampleRate)
     , blurKernels(new BlurKernel[KERNEL_COUNT], KERNEL_COUNT)
+    , feedbackFilterLeft(getSampleRate(), 20.f, 1.0f)
+    , feedbackFilterRight(getSampleRate(), 20.0f, 1.0f)
     , textureSizeSmoother(0.9f, MIN_TEXTURE_SIZE)
     , blurSizeSmoother(0.9f, MIN_BLUR_SIZE)
     , feedbackAmountSmoother(0.9f)
@@ -91,17 +97,30 @@ public:
   {
     // @todo high pass the feedback signal
     // to use CMSIS filters for this, we have to process feedback in blocks, maybe that's overkill here.
+    // Note: the way feedback is applied is based on how Clouds does it
     
     // Note: the way feedback is applied is based on how Clouds does it
     // see: https://github.com/pichenettes/eurorack/tree/master/clouds
     float fdbk = feedbackAmountSmoother.process(vessl::easing::interp<vessl::easing::quad::out>(0.f, 0.99f, *feedback()));
-    float slco = fdbk * 1.4f;
-    float inLeft = in.left();
-    float inRight = in.right();
+    float feedbackAmtLeft = fdbk;
+    float feedbackAmtRight = fdbk;
     float feedLeft = feedbackFrame.left();
     float feedRight = feedbackFrame.right();
-    float procLeft = inLeft + fdbk * (vessl::saturation::softlimit(slco*feedLeft + inLeft) - inLeft);
-    float procRight = inRight + fdbk * (vessl::saturation::softlimit(slco*feedRight + inRight) - inRight);
+
+    float cutoffL = (20.0f + 100.0f * feedbackAmtLeft * feedbackAmtLeft);
+    float cutoffR = (20.0f + 100.0f * feedbackAmtRight * feedbackAmtRight);
+    float slcoL = feedbackAmtLeft * 1.4f;
+    float slcoR = feedbackAmtRight * 1.4f;
+
+    feedbackFilterLeft.cutoff() << cutoffL;
+    feedbackFilterRight.cutoff() << cutoffR;
+    feedLeft = feedbackFilterLeft.process(feedLeft);
+    feedRight = feedbackFilterRight.process(feedRight);
+
+    float inLeft = in.left();
+    float inRight = in.right();
+    float procLeft = inLeft + fdbk * (vessl::saturation::softlimit(slcoL*feedLeft + inLeft) - inLeft);
+    float procRight = inRight + fdbk * (vessl::saturation::softlimit(slcoR*feedRight + inRight) - inRight);
     
     float tsz = textureSizeSmoother.process(vessl::easing::lerp<float>(MIN_TEXTURE_SIZE, MAX_TEXTURE_SIZE, *textureSize()));
     processorLeft->setTextureSize(tsz);
@@ -113,10 +132,10 @@ public:
     float blurHigh = blurLow + 1;
     BlurKernel::lerp(blurKernels[static_cast<int>(blurLow)], blurKernels[static_cast<int>(blurHigh)], blurFrac, processorLeft->getKernel());
     BlurKernel::lerp(blurKernels[static_cast<int>(blurLow)], blurKernels[static_cast<int>(blurHigh)], blurFrac, processorRight->getKernel());
-
-    float scale  = vessl::gain<float>::decibelsToScale(*gain());
+    
     GaussSampleFrame procOut = { processorLeft->process(procLeft), processorRight->process(procRight) };
     feedbackFrame = procOut;
+    float scale  = vessl::gain<float>::decibelsToScale(*gain());
     procOut.scale(scale);
     return procOut;
   }
