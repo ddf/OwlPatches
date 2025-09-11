3ba2eb806f446210c8dbfeedff999e3c603964ba
Damien Quartz <dcomposer@gmail.com>
2025-09-10 21:46:38 -0700

    added a freeze unit to vessl test that reads from the delay's delayline.

--- OwlPatches/VesslTestPatch.hpp
+++ OwlPatches/VesslTestPatch.hpp
@@ -13,6 +13,8 @@ using Delay = vessl::delay<float>;
 using Array = vessl::array<float>;
 using AudioReader = vessl::array<float>::reader;
 using AudioWriter = vessl::array<float>::writer;
+using FreezeBuffer = vessl::delayline<float>;
+using Freeze = vessl::freeze<float>;
 
 class VesslTestPatch final : public MonochromeScreenPatch
 {
@@ -25,11 +27,18 @@ class VesslTestPatch final : public MonochromeScreenPatch
   FloatArray delayBuffer;
   Delay delay;
   SmoothFloat delayTime;
+
+  FreezeBuffer freezeBuffer;
+  Freeze freeze;
+  StiffFloat freezeDelay;
+  StiffFloat freezeSize;
   
 public:
   VesslTestPatch() : wave(sine), osc(getSampleRate(), wave), voct(true), ramp(getSampleRate(), 0, 1, 0)
   , delayBuffer(FloatArray::create(static_cast<int>(getSampleRate())*2))
   , delay(Array(delayBuffer.getData(), delayBuffer.getSize()), getSampleRate(), 0.2f)
+  , freezeBuffer(delayBuffer.getData(), delayBuffer.getSize()), freeze(&freezeBuffer, getSampleRate())
+  , freezeDelay(getBlockSize()*4), freezeSize(getBlockSize()*4)
   {
     registerParameter(PARAMETER_A, ramp.duration().getName());
     setParameterValue(PARAMETER_A, 0.1f);
@@ -62,9 +71,16 @@ public:
 
     //delay.time() << vessl::duration::fromSeconds(getParameterValue(PARAMETER_A)*2.0f, getSampleRate());
     // note: HAS to be analog_t, otherwise the set by pointer conversion won't work.
-    delay.time() << static_cast<vessl::analog_t>(getParameterValue(PARAMETER_A)*2*getSampleRate());
-    delay.feedback() << getParameterValue(PARAMETER_B);
+    delay.time() << static_cast<vessl::analog_t>(getParameterValue(PARAMETER_E)*getSampleRate());
+    delay.feedback() << getParameterValue(PARAMETER_F);
 
+    // need to use StiffFloat or some other way of snapping size when using duration::mode::fade
+    freezeDelay = getParameterValue(PARAMETER_B)*getSampleRate();
+    freezeSize = vessl::easing::lerp(getSampleRate()/16, getSampleRate(), getParameterValue(PARAMETER_C));
+    freeze.position() << freezeDelay.getValue();
+    freeze.size() << freezeSize.getValue();
+    freeze.rate() << -1.0f + 2.0f * getParameterValue(PARAMETER_D);
+    
     uint16_t eorState = OFF;
     uint16_t eorIndex = 0;
     AudioReader pmIn(audioLeft);
@@ -86,10 +102,18 @@ public:
         eorIndex = eorState == OFF ? eorIndex + 1 : eorIndex;
       }
     }
-    delay.process<vessl::duration::mode::crossfade>(audioLeft, audioRight);
-    audioRight << audioLeft.add(audioRight).scale(0.5f);
-    
-    //audioRight << audioLeft;
+    if (freeze.enabled())
+    {
+      freeze.generate<vessl::duration::mode::fade>(audioLeft);
+    }
+    else
+    {
+      // still want to run freeze even when we don't use it for cleaner transitions
+      freeze.generate<vessl::duration::mode::snap>(audioRight);
+      delay.process<vessl::duration::mode::fade>(audioLeft, audioRight);
+      audioLeft.add(audioRight).scale(0.5f);
+    }
+    audioRight << audioLeft;
     
     setButton(BUTTON_1, eorState, eorIndex);
   }
@@ -100,6 +124,11 @@ public:
     {
       ramp.trigger();
     }
+
+    if (bid == BUTTON_2 && value == ON)
+    {
+      freeze.enabled() << !freeze.enabled().readBinary();
+    }
   }
   
   void processScreen(MonochromeScreenBuffer& screen) override
@@ -107,7 +136,7 @@ public:
     screen.setCursor(0, 10);
     //screen.setTextSize(2);
     // @todo this doesn't work for some reason, something is getting lost with the use of unitInit
-    screen.print(osc.name());
-    //screen.print("hello world");
+    //screen.print(osc.name());
+    screen.print("freeze: "); screen.print(freeze.enabled() ? "ON" : "OFF");
   }
 };
