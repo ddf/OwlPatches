b3c59d1dcfd3a893a78fd5760be687e64b128566
Damien Quartz <dcomposer@gmail.com>
2025-11-28 21:35:47 -0800

    update filter test to work with new filtering api

--- OwlPatches/VesslFilterTestPatch.hpp
+++ OwlPatches/VesslFilterTestPatch.hpp
@@ -5,7 +5,7 @@
 #include "vessl/vessl.h"
 
 using namespace vessl::filtering;
-using Filter = vessl::filter<float, biquad<2>::lowPass>;
+using Filter = vessl::filter<float, biquad<4>::lowPass>;
 
 class VesslFilterTestPatch : public MonochromeScreenPatch
 {
@@ -22,7 +22,7 @@ public:
 
   void processAudio(AudioBuffer& audio) override
   {
-    float cutoff = 120 + VoltsPerOctave::voltsToHertz(getParameterValue(PARAMETER_A)*5);
+    float cutoff = 60 + VoltsPerOctave::voltsToHertz(getParameterValue(PARAMETER_A)*4);
     float q = vessl::easing::lerp(q::butterworth<float>(), 5.0f, getParameterValue(PARAMETER_B));
     vessl::gain g = vessl::gain::fromDecibels(vessl::easing::lerp(-6.f, 6.f, getParameterValue(PARAMETER_C)));
     filter.cutoff() = cutoff;
@@ -32,18 +32,18 @@ public:
     vessl::array<float> inout(audio.getSamples(LEFT_CHANNEL), audio.getSize());
     inout >> filter >> inout;
 
-    float omega = vessl::math::pi<float>() * cutoff / getSampleRate();
-    filterFunc.set(omega, q, g);
-    //df2T.process(inout.getData(), inout.getData(), inout.getSize());
+    float dummy = 0;
+    args fargs(getSampleRate(), cutoff, q, g);
+    filterFunc.process(&dummy, &dummy, 1, fargs);
   }
 
   void processScreen(MonochromeScreenBuffer& screen) override
   {
     //screen.clear();
     screen.setCursor(0, 8);
-    screen.print("Coeff: "); screen.print((int)filterFunc.df2.getCoeffSize()); screen.print("\n");
-    screen.print("States: "); screen.print((int)filterFunc.df2.getStateSize()); screen.print("\n");
-    screen.print("Stages: "); screen.print((int)filterFunc.df2.getStageCount()); screen.print("\n");
+    screen.print("Coeff: "); screen.print(static_cast<int>(filterFunc.df2.getCoeffSize())); screen.print("\n");
+    screen.print("States: "); screen.print(static_cast<int>(filterFunc.df2.getStateSize())); screen.print("\n");
+    screen.print("Stages: "); screen.print(static_cast<int>(filterFunc.df2.getStageCount())); screen.print("\n");
     for (int i = 0; i < filterFunc.df2.getCoeffSize() / filterFunc.df2.getStageCount(); ++i)
     {
       screen.print(filterFunc.df2.coeff[i]);
